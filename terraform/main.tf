9c3759b2c7694f5f39cb51e195e2a3b2ef09c107
ovne <souza.v@aluno.ufabc.edu.br>
2023-05-17 13:10:31 -0300

    server vm definitions

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,62 +1,115 @@
 terraform {
   required_providers {
     google = {
-        source = "hashicorp/google"
-    }   
+      source = "hashicorp/google"
+    }
   }
 }
 
 provider "google" {
-    alias = "tokegen"
-    scopes = [ 
-        "https://www.googleapis.com/auth/cloud-platform",
-        "https://www.googleapis.com/auth/userinfo.email",
-    ] 
- }
+  alias = "tokegen"
+  scopes = [
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/userinfo.email",
+  ]
+}
 
 data "google_service_account_access_token" "SAToken" {
-    provider = google.tokegen
-    target_service_account = ""
-    scopes = ["userinfo-email", "cloud-platform"]
-    lifetime = "600s"
- }
+  provider               = google.tokegen
+  target_service_account = var.GCP_Project_Params.impersonation_service_acc
+  scopes                 = ["userinfo-email", "cloud-platform"]
+  lifetime               = "600s"
+}
 
 provider "google" {
-    project = ""
-    access_token = data.google_service_account_access_token.SAToken.access_token
-    request_timeout = "60s"
- }
+  project         = var.GCP_Project_Params.project_id
+  access_token    = data.google_service_account_access_token.SAToken.access_token
+  request_timeout = "60s"
+}
+
 
 variable "GCP_Project_Params" {
-    description = "a map of commomly used project params"
-    type = map(string)
-    default = {
-      "project_id" = "value"
-      "project_region" = "us-central1"
-      "project_zone" = "us-central1-a"
-      "impersonation_service_acc" = "cloud-adm@projeto-estudos-356715.iam.gserviceaccount.com"
-    } 
+  description = "a map of commomly used project params"
+  type        = map(string)
+  default = {
+    "project_id"                = "projeto-estudos-356715"
+    "project_region"            = "us-central1"
+    "project_zone"              = "us-central1-a"
+    "impersonation_service_acc" = "cloud-adm@projeto-estudos-356715.iam.gserviceaccount.com"
+  }
 }
 
-resource "" "name" {
-  
-}
 
+resource "google_compute_instance" "postgresql-server" {
+
+  machine_type = "e2-micro" // REQUIRED to free-tier
+  name         = "postgresql-server"
+  zone         = "us-central1-a"
+  boot_disk {
+    auto_delete = true
+    device_name = "postgresql-server"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-11-bullseye-v20230509"
+      size  = 10            //up to 30GB to free-tier
+      type  = "pd-standard" // REQUIRED to free-tier
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    ec-src = "vm_add-tf"
+  }
+
+  tags = ["allow-tcp-on-5432"] //network tags attached
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    subnetwork = "projects/projeto-estudos-356715/regions/us-central1/subnetworks/default"
+  }
+
+  scheduling {
+    automatic_restart   = true
+    on_host_maintenance = "MIGRATE"
+    preemptible         = false
+    provisioning_model  = "STANDARD"
+  }
+
+  service_account {
+    # email  = "872078393571-compute@developer.gserviceaccount.com"
+    email  = var.GCP_Project_Params.impersonation_service_acc
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+}
 
 module "firewall_rules" {
   source       = "terraform-google-modules/network/google//modules/firewall-rules"
   project_id   = var.GCP_Project_Params.project_id
-  network_name = module.vpc.network_name
+  network_name = "default"
 
   rules = [{
-    name                    = "allow-ingrees-postgres"
+    name                    = "test-allow-ingrees-postgres"
     description             = null
     direction               = "INGRESS"
     priority                = null
     ranges                  = ["0.0.0.0/0"]
     source_tags             = null
     source_service_accounts = null
-    target_tags             = "allow-tcp-on-5432"
+    target_tags             = ["allow-tcp-on-5432"]
     target_service_accounts = null
     allow = [{
       protocol = "tcp"
