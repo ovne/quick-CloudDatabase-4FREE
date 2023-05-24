472bd3d5038036b41dc0bd23fda1c62a644d9c90
ovne <souza.v@aluno.ufabc.edu.br>
2023-05-24 12:29:50 -0300

    improving script file

--- vm-startup-script.sh
+++ vm-startup-script.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-echo "=======Setting up the apt repository======="
+# =======Set up apt repository & install Docker engine=======
+# source: https://docs.docker.com/engine/install/debian/
 
-# 1) Update the apt package index and install packages to allow apt to use a repository over HTTPS:
 sudo apt-get update
+
 sudo apt-get install ca-certificates curl gnupg
 
-# 2) Add Docker’s official GPG key:
 sudo install -m 0755 -d /etc/apt/keyrings
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
-
-# 3) the following command to set up the repository:
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-
-echo "=======Installling Docker Engine======="
-
-# 1) Update the apt package index:
+# Installling Docker Engine
 sudo apt-get update
 
-# 2) Install Docker Engine, containerd, and Docker Compose.
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "=======Starting a PostgreSQL instance======="
-
+# =======Starting a PostgreSQL instance======="
 sudo docker pull postgres
 sudo docker run -d \
     --name postgresDB \
