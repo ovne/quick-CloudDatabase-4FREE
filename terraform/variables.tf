fb18d01f0afb57fe2b83f22aa7368ddfcb834085
ovne <souza.v@aluno.ufabc.edu.br>
2023-05-24 12:21:12 -0300

    add placeholders

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,9 +4,9 @@ variable "GCP_Project_Params" {
   description = "a map of commomly used project params"
   type        = map(string)
   default = {
-    "project_id"                = "projeto-estudos-356715"
+    "project_id"                = "<project-id>"
     "project_region"            = "us-central1"
     "project_zone"              = "us-central1-a"
-    "impersonation_service_acc" = "cloud-adm@projeto-estudos-356715.iam.gserviceaccount.com"
+    "impersonation_service_acc" = "<impersonatio-service-account-email>"
   }
 }
